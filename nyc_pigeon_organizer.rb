f7b93ae82b0c96401b6be1b38dd6c1e2a8d0f149
Andrew de la Vega <andrew21delavega@gmail.com>
2020-03-04 18:28:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,6 +13,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name][key] = []
         end
         
-        new_hash[name][key] << new_value.
+        new_hash[name][key] << new_value.to_s
+        
         
 end
